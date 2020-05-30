4b14beae7185793f0848531746733b0331826755
sserges <agounkeserge@gmail.com>
2020-05-30 19:21:42 +0000

    'Update product' controller added

--- deno-rest-api/controllers/products.ts
+++ deno-rest-api/controllers/products.ts
@@ -83,13 +83,43 @@ const addProduct = async (
 
 // @desc Update product
 // @route PUT /api/v1/products/:id
-const updateProduct = ({ response }: { response: any }) => {
-  response.body = "update product";
+const updateProduct = async (
+  { params, request, response }: {
+    params: { id: string };
+    request: any;
+    response: any;
+  },
+) => {
+  const product: Product | undefined = products.find((p) => p.id === params.id);
+
+  if (product) {
+    const body = await request.body();
+
+    const updateData: { name?: string; description?: string; price?: number } =
+      body.value;
+    products = products.map((p) =>
+      p.id === params.id ? { ...p, ...updateData } : p
+    );
+
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: products.find((p) => p.id === params.id),
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      msg: "No product found",
+    };
+  }
 };
 
 // @desc Delete product
 // @route DELETE /api/v1/products/:id
-const deleteProduct = ({ response }: { response: any }) => {
+const deleteProduct = (
+  { params, response }: { params: { id: string }; response: any },
+) => {
   response.body = "delete product";
 };
 
