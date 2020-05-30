304d0c4a9178969035aa3688e884aab811b25994
sserges <agounkeserge@gmail.com>
2020-05-30 11:51:04 +0000

    routes file added

--- deno-rest-api/server.ts
+++ deno-rest-api/server.ts
@@ -1,8 +1,9 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+import router from "./routes.ts";
 
 const port = 5000;
 const app = new Application();
-const router = new Router();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
