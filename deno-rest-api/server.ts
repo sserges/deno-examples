3bff0edfdd64b9ffa93a15991805711e9e83187b
sserges <agounkeserge@gmail.com>
2020-06-09 12:45:10 +0000

    Denon setup

--- deno-rest-api/server.ts
+++ deno-rest-api/server.ts
@@ -2,7 +2,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 
 import router from "./routes.ts";
 
-const port = 5000;
+const port = Deno.env.get("PORT") || 5000;
 const app = new Application();
 
 app.use(router.routes());
@@ -12,6 +12,8 @@ router.get("/api/v1/products", ({ response }: { response: any }) => {
   response.body = "Hello world";
 });
 
+// console.log(Deno.env.get("TOKEN"));
+
 console.log(`Server running on port ${port}`);
 
-await app.listen({ port });
+await app.listen({ port: +port });
