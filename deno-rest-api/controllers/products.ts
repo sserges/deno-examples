d3db8c6ab81c853704c81e718548066d1a5d2438
sserges <agounkeserge@gmail.com>
2020-06-09 23:53:10 +0000

    'Update one product' endpoint updated

--- deno-rest-api/controllers/products.ts
+++ deno-rest-api/controllers/products.ts
@@ -160,28 +160,53 @@ const updateProduct = async (
     response: any;
   },
 ) => {
-  const product: Product | undefined = products.find((p) => p.id === params.id);
+  await getProduct({ params: { "id": params.id }, response });
 
-  if (product) {
-    const body = await request.body();
-
-    const updateData: { name?: string; description?: string; price?: number } =
-      body.value;
-    products = products.map((p) =>
-      p.id === params.id ? { ...p, ...updateData } : p
-    );
-
-    response.status = 200;
-    response.body = {
-      success: true,
-      data: products.find((p) => p.id === params.id),
-    };
-  } else {
-    response.status = 404;
+  if (response.status === 404) {
     response.body = {
       success: false,
-      msg: "No product found",
+      msg: response.body.msg,
     };
+
+    response.status = 404;
+    return;
+  } else {
+    const body = await request.body();
+    const product = body.value;
+
+    if (!request.hasBody) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        msg: "No data",
+      };
+    } else {
+      try {
+        await client.connect();
+
+        const result = await client.query(
+          "UPDATE products SET name=$1, description=$2, price=$3 WHERE id=$4",
+          product.name,
+          product.description,
+          product.price,
+          params.id,
+        );
+
+        response.status = 200;
+        response.body = {
+          success: true,
+          data: product,
+        };
+      } catch (error) {
+        response.status = 500;
+        response.body = {
+          success: false,
+          msg: error.toString(),
+        };
+      } finally {
+        await client.end();
+      }
+    }
   }
 };
 
